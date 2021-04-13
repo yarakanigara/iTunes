9d19bacada583d7ef8c900a4f5c589275d210b5a
Yara Kanigara <yara.kanigara@gmail.com>
2021-04-14 00:00:15 +0700

    Update: Search Artist Container

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,43 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        // title: Text("iTunes"),
-        // centerTitle: true,
+      // Here we take the value from the MyHomePage object that was created by
+      // the App.build method, and use it to set our appbar title.
+      // title: Text("iTunes"),
+      // centerTitle: true,
       // ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,  
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.fromLTRB(
+                  50, 50, 50, 50 - MediaQuery.of(context).padding.top),
+              child: TextField(
+                maxLines: 1,
+                style: TextStyle(backgroundColor: Colors.white),
+                autofillHints: listOfArtist(),
+                onEditingComplete: searchArtistSongs(),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Search Artists',
+                    counterText: "",
+                    border: new OutlineInputBorder(
+                      borderSide: new BorderSide(color: Colors.blue)
+                    ),
+                    isDense: true,
+                    contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10)),
+              ),
+              decoration: BoxDecoration(
+                  border: BorderDirectional(
+                      bottom: BorderSide(color: Colors.black)),
+                  color: Colors.black12
+                  // boxShadow: [BoxShadow(color: Colors.black)]
+                  ),
+            ),
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -48,4 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  searchArtistSongs() {}
+  listOfArtist() {}
 }
