4410379aefdcfa5d47192ee64401eb0d28bc9617
Yara Kanigara <yara.kanigara@gmail.com>
2021-04-14 07:32:46 +0700

    Add: http lib & search artist function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+import 'package:i_tunes/songs.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,12 +28,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      // Here we take the value from the MyHomePage object that was created by
-      // the App.build method, and use it to set our appbar title.
-      // title: Text("iTunes"),
-      // centerTitle: true,
-      // ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -58,35 +56,37 @@ class _MyHomePageState extends State<MyHomePage> {
                     contentPadding: EdgeInsets.fromLTRB(8, 8, 8, 8)),
               ),
             ),
-            Container(
-              padding: getArtist() ? EdgeInsets.all(0) : EdgeInsets.fromLTRB(24, 8, 24, 8),
-              decoration: BoxDecoration(
-                border: BorderDirectional(
-                    bottom: BorderSide(color: Colors.black38)),
-                color: Colors.black12,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: getArtist() ? [] : [
-                  Text(
-                    "Artist Name",
-                    overflow: TextOverflow.fade,
-                    maxLines: 1,
-                    softWrap: false,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
-                  ),
-                  Text(
-                    "XX Songs",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                  ),
-                ],
-              ),
-            ),
+            // Container(
+            //   padding: getArtist() ? EdgeInsets.all(0) : EdgeInsets.fromLTRB(24, 8, 24, 8),
+            //   decoration: BoxDecoration(
+            //     border: BorderDirectional(
+            //         bottom: BorderSide(color: Colors.black38)),
+            //     color: Colors.black12,
+            //   ),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: getArtist() ? [] : [
+            //       Text(
+            //         "Artist Name",
+            //         overflow: TextOverflow.fade,
+            //         maxLines: 1,
+            //         softWrap: false,
+            //         style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+            //       ),
+            //       Text(
+            //         "XX Songs",
+            //         style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            //       ),
+            //     ],
+            //   ),
+            // ),
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [],
+                children: [
+                  Songs([{},{},{},{},{}])
+                ],    
               ),
             ),
             Container()
@@ -95,8 +95,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+  void searchArtists(String artist) async {
+    var url =
+      Uri.https('https://itunes.apple.com', '/lookup', {'q': '{http}'});
 
-  searchArtistSongs() {}
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      var itemCount = jsonResponse['totalItems'];
+      print('Number of books about http: $itemCount.');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
   listOfArtist() {}
   getArtist<bool>() {
     return true;
